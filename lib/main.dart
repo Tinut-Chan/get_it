141b91bb1936ba52e2cbac592f3a81fbf10a5d41
Tinut-Chan <tinutchan@gmail.com>
2023-04-20 13:18:45 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getit_project/router/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+    return GetMaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
     );
   }
 }
---
+++ lib/module/personal_profile/screen/personal_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PersonalProfileScreen extends StatelessWidget {
+  const PersonalProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('setting'),
+      ),
+    );
+  }
+}
